aa438414a05a548789b391c28b8b095a2783518f
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-20 14:39:21 +0200

    esercizio finito

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// chiedere quanti km deve percorrere il passeggero
+const kilometres = Number (prompt("inserire quanti kilometri si vogliono percorrere") )
+console.log(kilometres)
+// chiedere l'eta' del passeggero
+const age = Number (prompt("inserire l'eta'") )
+console.log(age)
+// calcolare il prezzo totale del viaggio 
+const ticket = kilometres * 0.21
+
+console.log(ticket)
+// se eta' < 18 appliccare il 20%
+// se eta' >= 65 applicare il 40%
+let ticketPrice
+if(age < 18){
+     ticketPrice = ticket - (ticket * 0.2)
+     document.getElementById("discount").innerHTML= ("20%")
+} else if (age >= 65){
+     ticketPrice = ticket - (ticket * 0.4)
+     document.getElementById("discount").innerHTML= ("40%")
+} else {
+     ticketPrice = ticket
+}
+console.log(ticketPrice)
+// stampo biglietto
+document.getElementById("kilometraggio").innerHTML= kilometres
+document.getElementById("price").innerHTML=ticketPrice
